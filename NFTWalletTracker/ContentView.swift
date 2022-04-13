d0c6d71a7d1d4012f3273ce7a11bc85821e60e97
Frank Pham <pham-frank@gmx.de>
2022-04-13 19:36:03 +0200

    Initial Commit

---
+++ NFTWalletTracker/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  NFTWalletTracker
+//
+//  Created by Frank Pham on 13.04.22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ NFTWalletTracker/NFTWalletTrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  NFTWalletTrackerApp.swift
+//  NFTWalletTracker
+//
+//  Created by Frank Pham on 13.04.22.
+//
+
+import SwiftUI
+
+@main
+struct NFTWalletTrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
